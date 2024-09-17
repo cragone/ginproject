a0d1adc642c391e2d8bdb52f94ef0d8b0f635664
cr816145 <ragonecharlie@gmail.com>
2024-09-16 21:59:20 -0400

    call the struct from the function, and finished new attendee handler

--- server/handlers/people_handlers.go
+++ server/handlers/people_handlers.go
@@ -7,6 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type NewAttendee struct {
+	FirstName   string `json:"f_name"`
+	LastName    string `json:"l_name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Rsvp        bool   `json:"rsvp"`
+}
+
 func HandleGetAllAttendees(c *gin.Context) {
 	fmt.Print("handler function running is get all attendees")
 
@@ -19,4 +27,29 @@ func HandleGetAllAttendees(c *gin.Context) {
 
 	fmt.Println("attendee list:", attendeeList)
 	c.JSON(200, gin.H{"attendees": attendeeList})
+}
+
+func HandleAddAttendee(c *gin.Context) {
+	fmt.Println("Running handler to add new attendee")
+
+	var newAttendee NewAttendee
+	if err := c.BindJSON(&newAttendee); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Call AddNewAttendee with a single AttendeeInfo struct
+	attendee, err := functions.AddNewAttendee(functions.AttendeeInfo{
+		FirstName:   newAttendee.FirstName,
+		LastName:    newAttendee.LastName,
+		Email:       newAttendee.Email,
+		PhoneNumber: newAttendee.PhoneNumber,
+		Rsvp:        newAttendee.Rsvp,
+	})
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"added_attendee": attendee})
 }

c89eccf989c3274fea10dccd9c74be62ed6bc3b1
Charles Ragone <charles.ragone@regeneron.com>
2024-09-19 11:05:23 -0400

    personal auth

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,8 @@ create table wedding (
 	wedding_id SERIAL PRIMARY KEY
 );
 
+
+--need to add an attendee serial id, for cases of the same name.
 create table attendees (
 	f_name VARCHAR(255),
 	l_name VARCHAR(255),
@@ -14,9 +16,27 @@ create table attendees (
 
 create table user_info (
 	user_email VARCHAR(255),
-	user_password VARCHAR(255),
+	user_fname VARCHAR(255),
+	user_lname VARCHAR(255),
+	hashpassword VARCHAR(255),
+	passwordreset VARCHAR(255) DEFAULT 'done',
 	wedding_id INT,
 	FOREIGN KEY (wedding_id) REFERENCES wedding(wedding_id)
 );
 
+
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    access_token TEXT NOT NULL,
+    refresh_token TEXT,
+    expiry TIMESTAMP
+);
 -- need to make it so the rsvp starts as null and is decided.
+
+
+
+-- CREATE TABLE tokens(
+--     email VARCHAR(255) NOT NULL,
+--     mytoken VARCHAR(255) PRIMARY KEY,
+--     FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE
+-- );

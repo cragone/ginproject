8f2ee247a84ba72664a592bfaa15ed4d1e620f73
cr816145 <ragonecharlie@gmail.com>
2024-09-20 19:15:59 -0400

    working login page

--- client/src/containers/LoginPage.jsx
+++ client/src/containers/LoginPage.jsx
@@ -6,24 +6,26 @@ const LoginPage = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent form submission
+  const handleSubmit = async () => {
+    console.log('handleSubmit called');
     const jsonData = {
-      UserEmail: userEmail,
-      UserPassword: userPassword, // Ensure this matches the expected key in your backend
+      user_email: userEmail,
+      hashpassword: userPassword
     };
-    console.log(jsonData);
-
+    console.log('jsonData:', jsonData);
+  
     try {
       const response = await axios.post(`http://${apiRoute}/auth/loggedin`, jsonData, {
         headers: {
           "Content-Type": "application/json",
         },
       });
+      console.log('Response:', response);
+
       setUserEmail("");
       setUserPassword("");
-      console.log(response.data); // Log user information to the console
       alert("You have logged in");
+
     } catch (error) {
       console.log("Incorrect password or username", error);
       alert("Incorrect password or username");
@@ -45,50 +47,48 @@ const LoginPage = () => {
         <div className="card-body">
           <h2 className="card-title text-center text-primary">Login</h2>
 
-          {/* Login Form */}
-          <form onSubmit={handleSubmit}>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text text-primary">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Email"
-                value={userEmail}
-                onChange={(e) => setUserEmail(e.target.value)}
-                className="input input-bordered border-secondary focus:border-accent w-full max-w-xs mb-4"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text text-primary">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Password"
-                value={userPassword}
-                onChange={(e) => setUserPassword(e.target.value)}
-                className="input input-bordered border-secondary focus:border-accent w-full max-w-xs mb-4"
-              />
-            </div>
+          {/* Input Fields */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-primary">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              className="input input-bordered border-secondary focus:border-accent w-full max-w-xs mb-4"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-primary">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+              className="input input-bordered border-secondary focus:border-accent w-full max-w-xs mb-4"
+            />
+          </div>
 
-            {/* Buttons */}
-            <div className="form-control mt-6 flex justify-between">
-              <button
-                type="submit"
-                className="btn btn-primary bg-primary hover:bg-primary-focus text-white w-full max-w-xs"
-                disabled={userEmail === "" || userPassword === ""}
-              >
-                Login
-              </button>
-              <a
-                href="/Register"
-                className="btn btn-outline text-primary border-primary hover:bg-accent"
-              >
-                Register here
-              </a>
-            </div>
-          </form>
+          {/* Buttons */}
+          <div className="form-control mt-6 flex justify-between">
+            <button
+              className="btn btn-primary bg-primary hover:bg-primary-focus text-white w-full max-w-xs"
+              disabled={userEmail === "" || userPassword === ""} // Disable if fields are empty
+              onClick={handleSubmit} // Handle submit
+            >
+              Login
+            </button>
+            <a
+              href="/Register"
+              className="btn btn-outline text-primary border-primary hover:bg-accent"
+            >
+              Register here
+            </a>
+          </div>
         </div>
       </div>
     </div>

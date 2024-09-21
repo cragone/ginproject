a637b841de1779d4c0e99f42ad9d106d7a86d0e9
cr816145 <ragonecharlie@gmail.com>
2024-09-21 18:25:17 -0400

    now caching wedding id and jwt

--- client/src/axios/ax.jsx
+++ client/src/axios/ax.jsx
@@ -1,3 +1,28 @@
-//need to make an axios that makes requests with a locally stored jwt.
-//need to also store and make requests with the wedding id.
-//i wonder if you can just use the wedding id as some form of authentication with the jwt
+import axios from 'axios';
+
+// Create an Axios instance
+const ax = axios.create({
+  baseURL: 'http://localhost:8080', // Set your base URL here
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to include the token in all requests
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // Get the token from localStorage
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // Add JWT token to headers
+    }
+
+    return config;
+  },
+  (error) => {
+    // Handle the error
+    return Promise.reject(error);
+  }
+);
+
+export default ax;
